02b7760c058fe99c84fde5538a98bbf619e981d3
kirill111111111 <155085103+kirill111111111@users.noreply.github.com>
2024-02-07 00:26:12 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение m:");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение n:");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = AckermannFunction(m, n);
+
+        Console.WriteLine($"Значение функции Аккермана для m={m} и n={n} равно {result}");
+
+        Console.ReadLine(); // Чтобы консольное окно не закрылось сразу
+    }
+
+    static int AckermannFunction(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return AckermannFunction(m - 1, 1);
+        }
+        else
+        {
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        }
+    }
+}
